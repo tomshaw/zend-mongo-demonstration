69750588b5c0b386dd9be9e16ced4ead2d146d5b
Tom Shaw <tom@tomshaw.info>
2013-01-01 00:02:22 -0600

    Bootstrap soft tabs.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,23 +2,24 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-    protected function _initHasMongo()
+
+    protected function _initHasMongo ()
     {
-        if(!extension_loaded('Mongo')) {
+        if (! extension_loaded('Mongo')) {
             throw new Exception('The PHP Mongo driver is not loaded.');
         }
     }
-	
+
     protected function _initAppAutoload ()
     {
         $loader = new Zend_Application_Module_Autoloader(array(
-        	'namespace' => '', 
-        	'basePath' => APPLICATION_PATH
+            'namespace' => '',
+            'basePath' => APPLICATION_PATH
         ));
         return $loader;
     }
-    
-    protected function _initView()
+
+    protected function _initView ()
     {
         $view = new Zend_View();
         
@@ -27,10 +28,8 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view->headTitle('Tom Shaw: Zend-MongoDB-Testing');
         
         $view->headLink()->appendStylesheet('/css/styles.css');
- 
-        $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper(
-            'ViewRenderer'
-        );
+        
+        $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
         
         $viewRenderer->setView($view);
         
